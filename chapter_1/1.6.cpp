7302ff250dbf8d924aeef04ba98e9786337e1ce6
DANG BA MINH <d.baminh@gmail.com>
2025-04-06 20:17:10 +1000

    feat: add Makefile and .clang-format

---
+++ chapter_1/1.6.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  int v1 = 1;
+  int v2 = 2;
+  // Remove the semicolon
+  std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2
+            << std::endl;
+}
